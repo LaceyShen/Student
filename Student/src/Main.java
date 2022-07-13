7bca16479dcc9741fca8de33b87cee04e3da8837
LaceyShen <lacey.shen@oocl.com>
2022-07-13 14:10:05 +0800

    add *

---
+++ Student/src/Main.java
@@ -0,0 +1,8 @@
+import model.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1=new Student("小王",18,"女",18);
+        System.out.println(student1.toString());
+    }
+}
---
+++ Student/src/model/Student.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Student {
+    String name;
+    int age;
+    String gender;
+    int ID_NO;
+
+    public Student(String name, int age, String gender, int ID_NO){
+        this.name=name;
+        this.age=age;
+        this.gender=gender;
+        this.ID_NO=ID_NO;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getID_NO() {
+        return ID_NO;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setID_NO(int ID_NO) {
+        this.ID_NO = ID_NO;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", ID_NO=" + ID_NO +
+                '}';
+    }
+}
